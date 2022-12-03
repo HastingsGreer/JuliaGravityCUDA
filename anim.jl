49ad8f48fd17d21803e4b6b3fad465c579480f16
Hastings Greer <hastings@hgreer.com>
2022-12-03 16:34:36 -0500

    newton gravity manual map reduce

--- anim.jl
+++ anim.jl
@@ -1,5 +1,6 @@
-using DynamicalSystems
-using OrdinaryDiffEq
+using FLoops, FoldsCUDA
+#using DynamicalSystems
+#using OrdinaryDiffEq
 using GLMakie
 using DataStructures: CircularBuffer
 using LinearAlgebra
@@ -8,74 +9,76 @@ using CUDA
 using GLMakie
 
 function doit()
-	N = 17588
+    N = 37588
 
-	px =CuArray( randn(N) )
-	py =CuArray( randn(N) ) 
-	pz =CuArray( randn(N) ) 
-	pz[100:end] .*= .1
-	vx =CuArray( (py) / 66 )
-	vy =CuArray( (-px) / 66 )
-	vz =CuArray( vx .* .1 )
+	#px =2 * (CuArray( Float32.(rand(N) )) .- .5f0)
+    #py =2 * (CuArray( Float32.(rand(N) )) .- .5f0)
+    #pz =2 * (CuArray( Float32.(rand(N) )) .- .5f0)
+	px =CuArray( Float32.(randn(N) ) )
+    py =CuArray( Float32.(randn(N) ) )
+    pz =CuArray( Float32.(randn(N) ) )
+    pz[100:end] .*= .4f0
+    vx = CuArray( (py) / 660 )
+    vy = CuArray( (-px) / 660 )
+    vz = CuArray( vx .* 1f-1 )
 
-	px_cpu = Array(px)
-	py_cpu = Array(py)
-
-
-	px_n = Observable(px_cpu)
-	py_n = Observable(py_cpu)
-
-	display(plot([0]))
+    px_cpu = Array(px)
+    py_cpu = Array(py)
 
 
-	scatter!(px_n, py_n, focus_on_show=false, markersize=1)
+    px_n = Observable(px_cpu)
+    py_n = Observable(py_cpu)
 
-	#ylims!(-1, 2)
-	#xlims!(-1, 2)
-	#
-	dx = CuArray(zeros(N, N))
-	dy = CuArray(zeros(N, N))
-	dz = CuArray(zeros(N, N))
-	dist = CuArray(zeros(N, N))
+	clear!()
+	
+	plot!([0], focus_on_show=false)
 
 
-	for t in 1:90
-	    px .= px .+ vx
-	    py .= py .+ vy
-	    pz .= pz .+ vz
-
-	    dx .= px .- px'
-	    dy .= py .- py'
-	    dz .= pz .- pz'
-
-	    
-	    dist .= (sqrt.(dx .^ 2 .+ dy .^ 2 .+ dz .^ 2) .+ .01) .^ (-3)
+    scatter!(px_n, py_n, focus_on_show=false, markersize=.5)
+	xlims!(-4, 4)
+	ylims!(-4, 4)
 
 
 
-	    dx .*= dist
-	    dy .*= dist
-	    dz .*= dist
+    for t in 1:60
+		px .= px + vx
+		py .= py + vy
+		pz .= pz + vz
+
+        @floop CUDAEx() for i in eachindex(vx, vy, vz)
+			fx = 0.0f1
+			fy = 0.0f1
+			fz = 0.0f1
+
+			for j in 1:N
+				dx = -px[i] + px[j]
+				dy = -py[i] + py[j]
+				dz = -pz[i] + pz[j]
+
+				dist = sqrt(dx^2 + dy ^2 + dz ^2)
+				dist = (dist + 1f-6 )^-3
+
+				dx *= dist
+				dy *= dist
+				dz *= dist
+
+				fx += dx
+				fy += dy
+				fz += dz
+			end
+			vx[i] = vx[i] + fx / 5000000000
+			vy[i] = vy[i] + fy / 5000000000
+			vz[i] = vz[i] + fz / 5000000000
+		end
 
 
-	    fxs = dropdims(sum(dx, dims=1), dims=1)
 
-	    fys = dropdims(sum(dy, dims=1), dims=1)
-	    fzs = dropdims(sum(dz, dims=1), dims=1)
+        px_cpu .= Array(px)
+		#py_cpu .= Array(pz)
+		py_cpu .= Array(py) .* .5 .+ Array(pz) .* sqrt(3) ./ 2
+	
 
-
-	    vx .= vx .+ fxs ./ 10000000
-	    vy .= vy .+ fys ./ 10000000
-	    vz .= vz .+ fzs ./ 10000000
-
-
-
-
-		px_cpu .= Array(px)
-		py_cpu .= Array(py)
-
-	    py_n[] = py_n[]
-	    sleep(0.01)
-	end
+        py_n[] = py_n[]
+    end
 end
-doit()
+@time doit()
